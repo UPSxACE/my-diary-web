438bc8c88354daa745caffc98424c9700d171e60
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2024-03-31 10:50:23 +0100

    chore: NEXT_BUILD_STANDALONE env variable

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
   },
+  // ... rest of the configuration.
+  output:
+    process.env.NEXT_BUILD_STANDALONE === "true" ? "standalone" : undefined,
 };
 
 export default nextConfig;
